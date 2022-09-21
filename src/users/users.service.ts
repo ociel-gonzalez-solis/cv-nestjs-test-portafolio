cf07db717ecb7b8d78901e07c33a8afda4ccb7d6
ociel <ocielgonzalezsolis@hotmail.com>
2022-09-21 16:44:57 -0700

    comment of the error

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -5,11 +5,16 @@ import { User } from './user.entity';
 
 @Injectable()
 export class UsersService {
+  // ! This Line below is breaking the tests in this projecy
   constructor(@InjectRepository(User) private repo: Repository<User>) {}
 
   create(email: string, password: string) {
     const user = this.repo.create({ email, password });
 
     return this.repo.save(user);
+  }
+
+  find(email: string) {
+    return this.repo.find({ where: { email } });
   }
 }
